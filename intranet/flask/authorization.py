bbc24b66171c8f39860e88d033adc6d4c637a90f
Schwifter <d.goglidze@sangu.edu.ge>
2024-05-18 23:54:19 -0700

    Extract authorization SQLite repository

--- intranet/flask/authorization.py
+++ intranet/flask/authorization.py
@@ -1,27 +1,11 @@
-import sqlite3
-from dataclasses import dataclass
-from sqlite3 import Connection
-from typing import ContextManager
-
 from flask import Blueprint, redirect, render_template, request, session
-from werkzeug.security import check_password_hash, generate_password_hash
 
 from intranet.error import apology
+from intranet.sqlite.authorization import AuthorizationSqliteRepository
 
 authorization = Blueprint(
     "authorization", __name__, template_folder="../front/templates"
 )
-
-
-@dataclass
-class SqliteConnector:
-    dsn: str = "database/database.db"
-
-    def connect(self) -> ContextManager[Connection]:
-        connection = sqlite3.connect(self.dsn)
-        connection.row_factory = sqlite3.Row
-
-        return connection
 
 
 @authorization.route("/login", methods=["GET", "POST"])
@@ -35,22 +19,7 @@ def login():  # type: ignore
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        with SqliteConnector().connect() as connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                "SELECT * FROM users WHERE username = :username;",
-                dict(username=request.form.get("username")),
-            )
-            rows = cursor.fetchall()
-
-            if len(rows) != 1 or not check_password_hash(
-                rows[0]["hash"],
-                request.form.get("password"),  # type: ignore
-            ):
-                return apology("invalid username and/or password", 403)
-
-            session["user_id"] = rows[0]["id"]
-            session["username"] = rows[0]["username"]
+        AuthorizationSqliteRepository().login()
 
         return redirect("/")
 
@@ -70,33 +39,7 @@ def register():  # type: ignore
     session.clear()
 
     if request.method == "POST":
-        with SqliteConnector().connect() as connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                "SELECT * FROM users WHERE username = :username;",
-                dict(username=request.form.get("username")),
-            )
-            rows = cursor.fetchall()
-
-            if not request.form.get("username"):
-                return apology("must provide username", 400)
-            elif len(rows) != 0:
-                return apology("username already exists", 400)
-            elif not request.form.get("password"):
-                return apology("must provide password", 400)
-            elif request.form.get("password") != request.form.get("confirmation"):
-                return apology("password didn't match", 400)
-
-            password = generate_password_hash(
-                str(request.form.get("password")),
-                method="pbkdf2",
-                salt_length=16,
-            )
-
-            cursor.execute(
-                "INSERT INTO users (username, hash) VALUES(:username, :hash);",
-                dict(username=request.form.get("username"), hash=password),
-            )
+        AuthorizationSqliteRepository().register()
 
         session.clear()
 
---
+++ intranet/sqlite/authorization.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+
+from flask import request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from intranet.error import apology
+from intranet.runner.factory import SqliteConnector
+
+
+@dataclass
+class AuthorizationSqliteRepository:
+    def login(self):
+        with SqliteConnector().connect() as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                "SELECT * FROM users WHERE username = :username;",
+                dict(username=request.form.get("username")),
+            )
+            rows = cursor.fetchall()
+
+            if len(rows) != 1 or not check_password_hash(
+                rows[0]["hash"],
+                request.form.get("password"),  # type: ignore
+            ):
+                return apology("invalid username and/or password", 403)
+
+            session["user_id"] = rows[0]["id"]
+            session["username"] = rows[0]["username"]
+
+    def register(self):
+        with SqliteConnector().connect() as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                "SELECT * FROM users WHERE username = :username;",
+                dict(username=request.form.get("username")),
+            )
+            rows = cursor.fetchall()
+
+            if not request.form.get("username"):
+                return apology("must provide username", 400)
+            elif len(rows) != 0:
+                return apology("username already exists", 400)
+            elif not request.form.get("password"):
+                return apology("must provide password", 400)
+            elif request.form.get("password") != request.form.get("confirmation"):
+                return apology("password didn't match", 400)
+
+            password = generate_password_hash(
+                str(request.form.get("password")),
+                method="pbkdf2",
+                salt_length=16,
+            )
+
+            cursor.execute(
+                "INSERT INTO users (username, hash) VALUES(:username, :hash);",
+                dict(username=request.form.get("username"), hash=password),
+            )
