1e9e57f71fa4b33a61cf4499e86e6cc97c6a670b
Schwifter <d.goglidze@sangu.edu.ge>
2024-05-24 00:12:06 -0700

    Add tests for home page

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,29 @@
-from intranet.hello import hello
+import pytest
+from flask.testing import FlaskClient
+
+from intranet.runner.cli import app
 
 
-def test_hello() -> None:
-    assert hello() == "Hello World!"
+@pytest.fixture()
+def client() -> FlaskClient:
+    client = app.test_client()
+
+    with client.session_transaction() as session:
+        session["user_id"] = "33b58955-d84b-40b0-b480-8416f519a567"
+
+    return client
+
+
+def test_home_with_authorization(client: FlaskClient) -> None:
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert b"Home | Intranet" in response.data
+
+
+def test_home_without_authorization(client: FlaskClient) -> None:
+    client.get("/logout")
+
+    response = client.get("/")
+
+    assert b"Redirecting..." in response.data
