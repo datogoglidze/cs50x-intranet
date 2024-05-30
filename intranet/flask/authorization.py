61586b4f37fd9016e13209877e6e654a6abbb262
Schwifter <d.goglidze@sangu.edu.ge>
2024-05-30 17:36:26 +0400

    Split and specify route methods

--- intranet/flask/authorization.py
+++ intranet/flask/authorization.py
@@ -1,7 +1,7 @@
 from uuid import uuid4
 
 from dependency_injector.wiring import Provide, inject
-from flask import Blueprint, current_app, redirect, render_template, request, session
+from flask import Blueprint, redirect, render_template, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from intranet.core.user import User, UserRepository
@@ -13,93 +13,97 @@ authorization = Blueprint(
 )
 
 
-@authorization.route("/login", methods=["GET", "POST"])
+@authorization.get("/login")
+def login_page():  # type: ignore
+    session.clear()
+
+    return render_template("login.html")
+
+
+@authorization.get("/register")
+def register_page():  # type: ignore
+    session.clear()
+
+    return render_template("register.html")
+
+
+@authorization.post("/login")
 @inject
 def login(users: UserRepository = Provide[Container.user_repository]):  # type: ignore
     session.clear()
 
-    if request.method == "POST":
-        user = User(
-            id=str(uuid4()),
-            username=request.form.get("username", ""),
-            password=request.form.get("password", ""),
-        )
+    user = User(
+        id=str(uuid4()),
+        username=request.form.get("username", ""),
+        password=request.form.get("password", ""),
+    )
 
-        if not user.username:
-            return apology("must provide username", 403)
+    if not user.username:
+        return apology("must provide username", 403)
 
-        if not user.password:
-            return apology("must provide password", 403)
+    if not user.password:
+        return apology("must provide password", 403)
 
-        try:
-            existing_user = users.read(user.username)
-        except KeyError:
-            return apology("invalid username and/or password", 403)
+    try:
+        existing_user = users.read(user.username)
+    except KeyError:
+        return apology("invalid username and/or password", 403)
 
-        if not check_password_hash(
-            existing_user.password,
-            user.password,
-        ):
-            return apology("invalid username and/or password", 403)
+    if not check_password_hash(
+        existing_user.password,
+        user.password,
+    ):
+        return apology("invalid username and/or password", 403)
 
-        session["user_id"] = existing_user.id
-        session["username"] = existing_user.username
+    session["user_id"] = existing_user.id
+    session["username"] = existing_user.username
 
-        return redirect("/")
-
-    else:
-        return render_template("login.html")
+    return redirect("/")
 
 
-@authorization.route("/logout")  # type: ignore
+@authorization.get("/logout")  # type: ignore
 def logout():
     session.clear()
 
     return redirect("/")
 
 
-@authorization.route("/register", methods=["GET", "POST"])
+@authorization.post("/register")
 @inject
 def register(users: UserRepository = Provide[Container.user_repository]):  # type: ignore
     session.clear()
 
-    current_app.logger.debug(f"users: {users}")
+    user = User(
+        id=str(uuid4()),
+        username=request.form.get("username", ""),
+        password=request.form.get("password", ""),
+    )
 
-    if request.method == "POST":
-        user = User(
-            id=str(uuid4()),
-            username=request.form.get("username", ""),
-            password=request.form.get("password", ""),
+    if not user.username:
+        return apology("must provide username", 403)
+
+    if not user.password:
+        return apology("must provide password", 403)
+
+    if user.password != request.form.get("confirmation", ""):
+        return apology("password didn't match", 403)
+
+    try:
+        users.create(
+            {
+                "username": user.username,
+                "password": (
+                    generate_password_hash(
+                        user.password,
+                        method="pbkdf2",
+                        salt_length=16,
+                    )
+                ),
+            }
         )
+    except ValueError:
+        return apology("username already exists", 403)
 
-        if not user.username:
-            return apology("must provide username", 403)
+    session.clear()
 
-        if not user.password:
-            return apology("must provide password", 403)
-
-        if user.password != request.form.get("confirmation", ""):
-            return apology("password didn't match", 403)
-
-        try:
-            users.create(
-                {
-                    "username": user.username,
-                    "password": (
-                        generate_password_hash(
-                            user.password,
-                            method="pbkdf2",
-                            salt_length=16,
-                        )
-                    ),
-                }
-            )
-        except ValueError:
-            return apology("username already exists", 403)
-
-        session.clear()
-
-        return redirect("/login")
-
-    else:
-        return render_template("register.html")
+    return redirect("/login")
--- intranet/flask/index.py
+++ intranet/flask/index.py
@@ -5,7 +5,7 @@ from intranet.error import login_required
 home = Blueprint("index", __name__, template_folder="../front/templates")
 
 
-@home.route("/")
+@home.get("/")
 @login_required  # type: ignore
-def index() -> str:
+def index_page() -> str:
     return render_template("index.html")
