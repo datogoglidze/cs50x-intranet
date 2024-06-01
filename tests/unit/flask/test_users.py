024f932b024b5cc0793a5e6d550c085a28309cb1
Schwifter <d.goglidze@sangu.edu.ge>
2024-06-01 00:08:08 -0700

    Use fake in tests

--- tests/unit/flask/test_users.py
+++ tests/unit/flask/test_users.py
@@ -4,6 +4,7 @@ import pytest
 
 from intranet.core.user import User
 from intranet.in_memory.users import UserInMemoryRepository
+from tests.fake import FakeUser
 
 
 @pytest.fixture()
@@ -13,24 +14,37 @@ def users() -> UserInMemoryRepository:
 
 def test_should_not_read_when_none_exist(users: UserInMemoryRepository) -> None:
     assert users.read_all() == []
+    assert len(users.read_all()) == 0
 
 
 def test_should_not_read_unknown(users: UserInMemoryRepository) -> None:
     with pytest.raises(KeyError):
-        users.read("datoie")
+        users.read(FakeUser().unknown_username())
 
 
 def test_should_persist(users: UserInMemoryRepository) -> None:
-    username = users.create({"username": "datoie", "password": "123"}).username
+    user = users.create(FakeUser().dict)
 
-    assert users.read(username) == User(id=ANY, username="datoie", password="123")
+    assert users.read(user.username) == User(
+        id=ANY,
+        username=user.username,
+        password=user.password,
+    )
 
 
 def test_should_persist_many(users: UserInMemoryRepository) -> None:
-    users.create({"username": "datoie", "password": "123"})
-    users.create({"username": "datoie2", "password": "123"})
+    user_1 = users.create(FakeUser().dict)
+    user_2 = users.create(FakeUser().dict)
 
     assert users.read_all() == [
-        User(id=ANY, username="datoie", password="123"),
-        User(id=ANY, username="datoie2", password="123"),
+        User(id=ANY, username=user_1.username, password=user_1.password),
+        User(id=ANY, username=user_2.username, password=user_2.password),
     ]
+
+
+def test_should_not_duplicate(users: UserInMemoryRepository) -> None:
+    user = FakeUser().dict
+    users.create(user)
+
+    with pytest.raises(ValueError):
+        users.create(user)
