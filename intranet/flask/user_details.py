1591a21796cabdd726cfe7c24a04e0933bf18ead
Schwifter <d.goglidze@sangu.edu.ge>
2024-06-07 22:02:45 -0700

    Add display page

---
+++ intranet/flask/user_details.py
@@ -0,0 +1,22 @@
+from uuid import uuid4
+
+from dependency_injector.wiring import Provide, inject
+from flask import Blueprint, redirect, render_template, request, session
+
+from intranet.core.user import User, UserRepository
+from intranet.core.user_details import UserDetailsRepository
+from intranet.error import apology, login_required
+from intranet.flask.dependable import Container
+
+user_details = Blueprint("user_details", __name__, template_folder="../front/templates")
+
+
+@user_details.get("/user-details")
+@inject
+@login_required  # type: ignore
+def user_details_page(
+    details: UserDetailsRepository = Provide[Container.user_details_repository],
+):  # type: ignore
+    _user_details = details.read(session["user_id"])
+
+    return render_template("user_details.html", user_details=_user_details)
