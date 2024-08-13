bcfed1f3069876c397ec726e87262f5c5c27bfee
Schwifter <d.goglidze@sangu.edu.ge>
2024-08-13 00:28:50 -0700

    Move news creation on homepage

--- intranet/flask/news.py
+++ intranet/flask/news.py
@@ -23,12 +23,6 @@ def news_page(
     return render_template("index.html", news=news_items)
 
 
-@news.get("/news")
-@login_required
-def add_news_page() -> str:
-    return render_template("news.html")
-
-
 @news.post("/news")
 @inject
 @login_required
