f1d91b838bbce8e0c205fa810e3a7b1ca7aee609
Schwifter <d.goglidze@sangu.edu.ge>
2024-08-27 21:53:54 +0400

    Add Todo

---
+++ TODO.py
@@ -0,0 +1,6 @@
+# TODO:
+#   1. Make pretty "About Us" page;
+#   2. Display "User Details" page in two columns;
+#   3. Make spacing between"User Document" and navbar smaller;
+#   4. Add ability to publish news;
+#   5. Add administrator actions (Approve documents and publish news).
