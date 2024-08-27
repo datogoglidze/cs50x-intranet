3156f6b2378561ad54a4ad2e21552b01f330c45f
Schwifter <d.goglidze@sangu.edu.ge>
2024-08-28 01:22:38 +0400

    Hardcode repository mode

--- intranet/flask/dependable.py
+++ intranet/flask/dependable.py
@@ -1,11 +1,11 @@
 from dependency_injector import containers, providers
 
 from intranet.in_memory.news import NewsInMemoryRepository
+from intranet.in_memory.users import UserInMemoryRepository
 from intranet.in_memory.users_details import UsersDetailsInMemoryRepository
-from intranet.mssql.users import UserMssqlRepository
 
 
 class Container(containers.DeclarativeContainer):
-    user_repository = providers.Singleton(UserMssqlRepository)
+    user_repository = providers.Singleton(UserInMemoryRepository)
     user_details_repository = providers.Singleton(UsersDetailsInMemoryRepository)
     news_repository = providers.Singleton(NewsInMemoryRepository)
