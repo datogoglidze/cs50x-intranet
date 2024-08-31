38dc4155a71f62ec3b10f08322425e9e5e633b26
Schwifter <d.goglidze@sangu.edu.ge>
2024-08-31 22:49:39 +0400

    Add ability to chose repository

--- intranet/flask/dependable.py
+++ intranet/flask/dependable.py
@@ -1,11 +1,28 @@
+import os
+
+from click import echo
 from dependency_injector import containers, providers
+from dotenv import load_dotenv
 
 from intranet.in_memory.news import NewsInMemoryRepository
 from intranet.in_memory.users import UserInMemoryRepository
 from intranet.in_memory.users_details import UsersDetailsInMemoryRepository
+from intranet.mssql.users import UserMssqlRepository
 
 
 class Container(containers.DeclarativeContainer):
-    user_repository = providers.Singleton(UserInMemoryRepository)
+    load_dotenv()
+
+    if "DB_HOST" in os.environ:
+        echo("Using MsSQL repository")
+    else:
+        echo("Using In-Memory repository")
+
+    user_repository = providers.Singleton(
+        lambda: UserMssqlRepository()
+        if "DB_HOST" in os.environ
+        else UserInMemoryRepository()
+    )
+
     user_details_repository = providers.Singleton(UsersDetailsInMemoryRepository)
     news_repository = providers.Singleton(NewsInMemoryRepository)
