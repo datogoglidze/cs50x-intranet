03b2af153054888e2f4e6cb5d77549e1de3b9b4e
Schwifter <d.goglidze@sangu.edu.ge>
2024-09-09 12:01:59 +0400

    Edit news styling

--- TODO.py
+++ TODO.py
@@ -2,5 +2,4 @@
 #   1. Make pretty "About Us" page;
 #   2. Display "User Details" page in two columns;
 #   3. Make spacing between"User Document" and navbar smaller;
-#   4. Add ability to publish news;
 #   5. Add administrator actions (Approve documents and publish news).
