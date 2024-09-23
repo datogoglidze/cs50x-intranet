4014e79c591ada58663c6a51fed3e904429d55ab
Schwifter <d.goglidze@sangu.edu.ge>
2024-09-24 00:11:14 +0400

    Edit date type in MsSQL

--- intranet/mssql/scripts/user_details.sql
+++ intranet/mssql/scripts/user_details.sql
@@ -3,7 +3,7 @@ CREATE TABLE user_details
      [id]           [NVARCHAR](36)  NOT NULL PRIMARY KEY,
      [first_name]   [NVARCHAR](100),
      [last_name]    [NVARCHAR](100),
-     [birth_date]   [INT],
+     [birth_date]   [NVARCHAR](10),
      [department]   [NVARCHAR](100),
      [email]        [NVARCHAR](100),
      [phone_number] [NVARCHAR](100),
